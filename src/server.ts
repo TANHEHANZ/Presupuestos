a01bf413e3d57686364b3659fa87311ed613178b
TANHEHANZ <hantach10@gmail.com>
2025-06-09 13:38:55 -0400

    feat(estructuring):manage folders and architecture

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import appovent from "../routes";
+
+export const createServer = () => {
+  const app = express();
+
+  app
+    .disable("x-powered-by")
+    .use(express.urlencoded({ extended: true }))
+    .use(express.json({ limit: "50mb" }))
+    .use(express.json())
+    .use(cors())
+    .use("/v1/api/", appovent);
+
+  app.get("/", (req: Request, res: Response) => {
+    res.json({
+      status: "success",
+      message: "Appovent Service is running",
+      timestamp: new Date().toISOString(),
+    });
+  });
+
+  return app;
+};
