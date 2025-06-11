9c15dec9a99665bc1ced66fa3351fa93788fae2a
TANHEHANZ <hantach10@gmail.com>
2025-06-11 13:45:55 -0400

    fix(tailwind): correct color values and formatting in tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors: {
-        'primary': '#F18721',     
-        'secondary': '#F59E0B', 
-        'accent': '#10B981', 
-        'background': '#F3F4F6', 
-        'text': '#111827', 
-      }
+        primary: "#F18721",
+        secondary: "#FFD399",
+        olther: "#6C5BA7",
+        danger: "#B1124D",
+        accent: "#10B981",
+        background: "#F3F4F6",
+        text: "#111827",
+      },
     },
   },
   plugins: [],
-}
+};
