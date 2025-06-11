03c9ea2d882f25f80ee57cf17d7f0ae9fea55d7e
TANHEHANZ <hantach10@gmail.com>
2025-06-11 11:01:58 -0400

    feat(login): implement reactive form for login and enhance animation transitions

--- frontend/src/app/precentation/pages/public/login/login.component.ts
+++ frontend/src/app/precentation/pages/public/login/login.component.ts
@@ -8,62 +8,159 @@ import {
 import { gsap } from 'gsap';
 import { TextPlugin } from 'gsap/TextPlugin';
 import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
+import { CustomInputComponent } from '../../../shared/input/input.component';
 gsap.registerPlugin(TextPlugin);
 @Component({
   selector: 'app-login',
   template: `
     <div
-      class="h-screen flex items-center justify-center gap-4 bg-gray-100 relative overflow-hidden "
+      class="h-screen flex items-center justify-end gap-4 bg-gray-100 relative overflow-hidden "
     >
       <div
         #bloque
-        class="bloque w-[40dvw] h-screen bg-primary  -skew-x-6    left-0   transition-all duration-1000 ease-in-out justify-center items-center "
-      ></div>
-      <form
-        #loginForm
-        (submit)="onLogin($event)"
-        class="p-6 sm:p-8 rounded w-full max-w-xs sm:max-w-sm md:max-w-xl flex flex-col gap-4 z-10  relative"
+        class="bloque w-[40dvw] h-screen bg-primary  -skew-x-[12deg] flex  justify-center items-center absolute left-[10dvw] "
       >
-        <h1 class="text-primary text-5xl font-bold text-center mb-4">
-          Iniciar sesión
-        </h1>
-        <div class="flex flex-col">
-          <label for="usuario" class="ml-2">CI</label>
-          <input
-            type="text"
-            placeholder="Ingrese su cédula de identidad"
-            class="border border-primary rounded-xl px-3 py-4 focus:outline-none"
-          />
-        </div>
-        <div class="flex flex-col">
-          <label for="contraseña" class="ml-2">Contraseña</label>
-          <input
-            type="password"
-            placeholder="Contraseña"
-            class="border border-primary rounded-xl px-3 py-4 focus:outline-none"
-          />
-        </div>
-        <button
-          type="submit"
-          class="bg-primary text-white rounded-xl px-3 py-4 font-semibold"
+        <div
+          class="logo text-white  skew-x-[12deg] w-full h-full relative flex justify-center items-center flex-col gap-4"
         >
-          Entrar
-        </button>
-      </form>
+          <img
+            src="/logoBK.png"
+            alt="Logo"
+            class="logo aspect-square w-44 "
+            #logoImg
+          />
+          <section class="carga hidden scale-0 absolute bottom-[30%]">
+            <div class="spinner">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </section>
+        </div>
+      </div>
+      <section class="w-1/2 flex justify-center items-center">
+        <form
+          #loginForm
+          (submit)="onLogin($event)"
+          class="p-6 sm:p-8 rounded w-[60%]  flex flex-col gap-4 z-10  relative "
+        >
+          <h1 class="text-primary text-5xl font-bold text-center mb-4">
+            Iniciar sesión
+          </h1>
+          <app-custom-input
+            label="CI"
+            formControlName="ci"
+            placeholder="Ingrese su cédula de identidad"
+          />
+          <app-custom-input
+            label="Contraseña"
+            type="password"
+            formControlName="password"
+            placeholder="Contraseña"
+          />
+          <button
+            type="submit"
+            class="bg-primary text-white rounded-xl px-3 py-4 font-semibold"
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
     </div>
   `,
+  imports: [CustomInputComponent],
 })
 export class LoginComponent implements OnInit {
   @ViewChild('bloque') bloque!: ElementRef;
   @ViewChild('loginForm') loginForm!: ElementRef;
   router = inject(Router);
+
+  form = new FormGroup({
+    ci: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
   ngOnInit(): void {
-    gsap.from('.bloque', {
-      duration: 1,
-      translateX: -1000,
-      ease: 'ease-in-out',
-    });
+    gsap.fromTo(
+      '.bloque',
+      {
+        duration: 1,
+
+        translateX: -1000,
+        ease: 'ease-in-out',
+      },
+      {
+        duration: 1,
+        translateX: 0,
+        ease: 'ease-in-out',
+      }
+    );
   }
+
+  //   onLogin(event: Event) {
+  //     event.preventDefault();
+
+  //     const tl = gsap.timeline({
+  //       // onComplete: () => {
+  //       //   setTimeout(() => {
+  //       //     this.router.navigate(['/dashboard/presupuestos']);
+  //       //   }, 500);
+  //       // },
+  //     });
+  //     tl.to('.logo', {
+  //       opacity: 0,
+  //       scale: 0.8,
+  //       duration: 0.5,
+  //       ease: 'power2.inOut',
+  //     });
+
+  //     tl.to(this.bloque.nativeElement, {
+  //       skewX: 0,
+  //       rotation: 90,
+  //       height: '3300px',
+  //       width: '100vw',
+  //       duration: 1,
+  //       ease: 'power2.inOut',
+  //       onComplete: () => {
+  //         gsap.to('.carga', {
+  //           scale: 1,
+  //           opacity: 1,
+  //           duration: 0.5,
+  //           ease: 'power2.inOut',
+  //         });
+  //       },
+  //     })
+
+  //       .to(
+  //         this.loginForm.nativeElement,
+  //         {
+  //           opacity: 0,
+  //           overflow: 'hidden',
+  //           duration: 1,
+  //           ease: 'power2.inOut',
+  //         },
+  //         '<'
+  //       )
+  //       .to(this.bloque.nativeElement, {
+  //         y: -1400,
+  //         duration: 1.5,
+  //         delay: 0.8,
+  //         ease: 'power2.inOut',
+  //       });
+  //   }
+  // }
   onLogin(event: Event) {
     event.preventDefault();
 
@@ -74,30 +171,50 @@ export class LoginComponent implements OnInit {
         }, 500);
       },
     });
+    tl.to(this.loginForm.nativeElement, {
+      opacity: 0,
+      duration: 0.5,
+      ease: 'power2.inOut',
+      display: 'none',
+    });
+    tl.to('.logo', {
+      skewX: 0,
+      duration: 0.5,
+      scale: 1.1,
+      ease: 'power2.inOut',
+    });
+    tl.to(
+      this.bloque.nativeElement,
+      {
+        skewX: 0,
+        left: 0,
+        duration: 0.8,
+        height: '20vw',
+        width: '100vw',
+        ease: 'power2.inOut',
+      },
+      '<'
+    );
 
     tl.to(this.bloque.nativeElement, {
-      skewX: 0,
-      rotation: 90,
-      height: '3000px',
-      width: '100vw',
-      duration: 1,
+      height: '100vh',
+      duration: 1.5,
       ease: 'power2.inOut',
-    })
-      .to(
-        this.loginForm.nativeElement,
-        {
-          opacity: 0,
-          overflow: 'hidden',
-          duration: 1,
-          ease: 'power2.inOut',
-        },
-        '<'
-      )
-      .to(this.bloque.nativeElement, {
-        y: -1400,
-        duration: 1.5,
-        delay: 0.8,
-        ease: 'power2.inOut',
-      });
+    });
+
+    tl.to('.carga', {
+      scale: 1,
+      opacity: 1,
+      duration: 0.5,
+      ease: 'power2.inOut',
+      display: 'flex',
+    });
+    tl.to(this.bloque.nativeElement, {
+      translateY: -3000,
+      ease: 'power2.inOut',
+      duration: 0.5,
+      delay: 2,
+      display: 'none',
+    });
   }
 }
