696423b82484cfa59fad681a53dc93b572561364
TANHEHANZ <hantach10@gmail.com>
2025-06-12 11:18:40 -0400

    feat(container):add component container for ui standar

---
+++ frontend/src/app/precentation/shared/container/container.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  template: `
+    <section
+      class="bg-white rounded-lg p-8 flex flex-col gap-2 overflow-hidden"
+      [ngStyle]="{ height: h, width: w }"
+    >
+      <p
+        class="text-[9px] uppercase bg-olther/15 text-olther font-semibold self-start px-2 py-1 rounded-lg"
+      >
+        {{ title }}
+      </p>
+      <div>
+        <ng-content></ng-content>
+      </div>
+    </section>
+  `,
+  imports: [CommonModule],
+})
+export class ContainerComponent {
+  @Input() title: string = '';
+  @Input() h: string = '100%';
+  @Input() w: string = '100%';
+}
