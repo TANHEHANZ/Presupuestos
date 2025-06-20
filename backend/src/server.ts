7de8d14c2e1eb29a15dae3fb738f430a45a52858
TANHEHANZ <hantach10@gmail.com>
2025-06-20 14:52:43 -0400

    feat(coors):change coors for cookies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,14 +4,21 @@ import routes from "./modules/routes";
 import path from "path";
 import cookieParser from "cookie-parser";
 export const createServer = () => {
+  const allowedOrigin = "http://localhost:4200";
   const app = express();
   app.use(cookieParser());
   app
     .disable("x-powered-by")
     .use(express.urlencoded({ extended: true }))
     .use(express.json({ limit: "100mb" }))
-    .use(express.json())
-    .use(cors())
+    .use(express.json());
+  app
+    .use(
+      cors({
+        origin: allowedOrigin,
+        credentials: true,
+      })
+    )
     .use("/v1/api/", routes)
     .use(express.static(path.join(__dirname, "../public")));
 
