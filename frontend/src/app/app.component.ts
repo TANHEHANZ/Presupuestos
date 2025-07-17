1573b813a6169915a73ae994272b0bb492deaedb
TANHEHANZ <hantach10@gmail.com>
2025-07-17 14:16:52 -0400

    refactor(.infra):reorganitation of files deploy

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from './infraestructure/config/environment';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `<router-outlet />`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  environment = environment;
+  ngOnInit(): void {
+    console.log('ENV TEST:', environment.API_BACK);
+    console.log('PRODUCTIONS:', environment.production);
+  }
 }
---
+++ frontend/src/app/infraestructure/config/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_BACK: 'https://mi servidor.com',
+};
